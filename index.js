e36ba8a960fa0f603483a9f8b7255711b7adb604
cleber1812 <cleber1812roberto@gmail.com>
2021-12-10 18:48:52 -0300

    Restaurando configurações do Servidor Local

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ const app = express();
 app.use(express.urlencoded({extended: false}));
 app.use(routes);
 app.listen(3000);
+
+// app.listen((process.env.PORT || 3000), function(){
+//     console.log('listening on *:3000');
+//   });
