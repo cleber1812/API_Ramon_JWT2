52e5269fffe9fd8b05d233b0421651c4a2df0ff9
cleber1812 <cleber1812roberto@gmail.com>
2021-12-11 08:24:59 -0300

    Configurações de Lucas

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const routes = require('./routes/routes');
-require('dotenv').config({path: './config/.env'});
 const app = express();
 
+require('dotenv').config({path: './config/.env'});
+
+app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(routes);
-app.listen(3000);
+
+// app.listen(3000);
 
 // app.listen((process.env.PORT || 3000), function(){
-//     console.log('listening on *:3000');
-//   });
+// console.log('listening on *:3000');
+// });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.warn(`App listening on http://localhost:${PORT}`);
+});
